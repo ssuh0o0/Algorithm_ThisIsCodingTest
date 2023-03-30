2ea2d4e2d2c2621e9d09a75b46ddf43a7f71d881
soo <ssuh0o0@daum.net>
2023-03-31 00:47:40 +0900

    2023/03/30 dfs bfs

--- DFS BFS/연구소/soohee.py
+++ DFS BFS/연구소/soohee.py
@@ -1,20 +1,43 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
 graph = []
 for i in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int, input().split())))
 
-cnt = 0
-def dfs(graph, x, y):
 
-    if ( 0 <= x and x < n ) and (0 <= y and y < m ):
-        if graph[x][y] == 0:
-            graph[x][y] = 2
-            dfs(graph, x-1, y)
-            dfs(graph, x+1, y)
-            dfs(graph, x, y-1)
-            dfs(graph, x, y+1)
-            return True
-    return False
-    
-print(cnt)
+dx = [1, 0, -1, 0 ] 
+dy = [0, 1, 0, -1] 
+
+def dfs(cnt):
+    global zeroCnt
+
+    # 벽 3개 생성 완료
+    if cnt == 3:
+        _graph = graph[:]
+
+        virus = 0
+        for y in range(n):
+            for x in range(m):
+                if _graph[y][x] == 2:
+                    for i in range(4):
+                        ny = y+dy[i]
+                        nx = x+dx[i]
+                        if ny >= 0 and nx >= 0 and ny < n and nx < m:
+                           if _graph[ny][nx] == 0:
+                                _graph[y][x] = 2
+                                virus += 1
+        return
+
+    # 벽 만듦
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                cnt += 1
+                dfs(cnt)
+                graph[i][j] = 0
+                cnt -= 1
+
--- DFS BFS/음료수 얼려 먹기/soohee.py
+++ DFS BFS/음료수 얼려 먹기/soohee.py
@@ -3,51 +3,26 @@ n, m = map(int, input().split())
 
 graph = []
 for i in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int,input().strip())))
 
-##### dfs
+dx = [1, 0, -1, 0 ] # 오른쪽 아래 왼쪽 위
+dy = [0, 1, 0, -1] 
 
-# def dfs(graph, x, y):
-#     if ( 0 <= x and x < n ) and (0 <= y and y < m ):
-#         if graph[x][y] == 0:
-#             graph[x][y] = 2
-#             dfs(graph, x-1, y)
-#             dfs(graph, x+1, y)
-#             dfs(graph, x, y-1)
-#             dfs(graph, x, y+1)
-#             return True
-#     return False
+def dfs(y, x):
+    if y >= 0 and x >= 0 and y < n and x < m:
+        if graph[y][x] == 0:
+            graph[y][x] = 2
+            for i in range(4):
+                dfs(y+dy[i], x+dx[i])
+            return True
+    return False
     
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i , j):
+            cnt += 1
 
-# cnt = 0
-# for i in range(n):
-#     for j in range(m):
-#         if dfs(graph, i , j):
-#             cnt += 1
-# print(cnt)
-
-
-##### bfs
-
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1] 
-
-def bfs(x, y): 
-    cnt = 0
-    queue = deque()
-    queue.append((x,y)) 
-    while queue: 
-        x, y = queue.popleft() 
-        for i in range(4): 
-            nx, ny = x + dx[i], y + dy[i] 
-            if nx < 0 or  nx >= n or ny < 0  or ny >= m: 
-                continue 
-            if graph[nx][ny] == 1: 
-                continue 
-            if graph[nx][ny] == 0: 
-                graph[nx][ny] = graph[x][y] + 1 
-                queue.append((nx, ny)) 
-    return cnt 
-print(bfs(0, 0))
+print(cnt)
 
     
