3f3139badd5a5d306ad3f6b47204ef6eb07e0e9b
soo <ssuh0o0@daum.net>
2023-05-02 11:37:56 +0900

    2023/05/02 DP

---
+++ DP/퇴사.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+work = []
+for _ in range(n):
+    work.append(list(map(int, input().split())))
+dp = [0] * (n+1)
+
+# for i in range(n-1, -1, -1):
+#     if i + work[i][0] > n:
+#         dp[i] = dp[i+1]
+#     else:
+#         dp[i] = max(dp[i+1], dp[i+work[i][0]] + work[i][1])
+
+for i in range(n):
+    for j in range(i+work[i][0], n+1):
+        dp[j] = max(dp[j], dp[i] + work[i][1])
+
+print(dp[-1])
